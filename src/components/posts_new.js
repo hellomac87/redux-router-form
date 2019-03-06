844503acb73525ce27da43d3fc1748fd54ef02c3
hellomac87 <hellomac87@gmail.com>
2019-03-06 23:53:00 +0900

    redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { reduxForm } from "redux-form";
 
 class PostsNew extends Component {
   render() {
     return (
       <form>
-        <h1>Create Form</h1>
+        <h3>Create a new post</h3>
+        <div className="form-group" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
       </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: "PostsNewForm",
+  fields: ["title", "categories", "content"]
+})(PostsNew);
